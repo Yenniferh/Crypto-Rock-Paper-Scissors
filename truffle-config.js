f8b938cebf7224b040b4352007083dffc2cf3052
Yennifer Herrera <yenniferh@uninorte.edu.co>
2022-03-18 13:48:04 -0500

    UPD: Set up for deployment to Rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -14,7 +16,24 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*"
-    }
+    },
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: process.env.MNEMONIC,
+          },
+          providerOrUrl: process.env.RPC_URL,
+          numberOfAddresses: 1,
+          shareNonce: true,
+          derivationPath: "m/44'/1'/0'/0/"
+        }),
+      network_id: 4,       // Rinkeby's id
+      gas: 5500000,        // Rinkeby has a lower block limit than mainnet
+      confirmations: 3,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
   },
   compilers: {
     solc: {
